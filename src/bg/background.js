f7ccc4abf7925855734c5c106b76420c99ba3ae0
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-10-18 22:32:58 +0530

    Add specs.txt and add permissions for page_action.html

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,13 +1,14 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log('The color is green.');
   });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'www.airbnb.co.in'},
+      })
+      ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
